d950492224b7ff620d19feee23f766c73c14e29b
Alex Bepple <alex@bepple.de>
2014-11-18 20:08:16 +0100

    do not serialize statements
    There seems to be no need.

--- generate_index.js
+++ generate_index.js
@@ -1,5 +1,4 @@
 var fs = require('fs');
-var r = require('ramda');
 var cheerio = require('cheerio');
 var sqlite3 = require('sqlite3');
 var db = new sqlite3.Database('build/Ramda.docset/Contents/Resources/docSet.dsidx');
@@ -19,9 +18,7 @@ fs.readFile('./build/Ramda.docset/Contents/Resources/Documents/R.html', { encodi
     var functionNames = [];
     $('h4.name').each(function(_, elem){ functionNames.push(elem.attribs.id); });
 
-    db.serialize(function(){
-        functionNames.forEach(function(functionName){
-            db.run('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ("' + functionName + '", "Function", "R.html#' + functionName + '");');
-        });
+    functionNames.forEach(function(functionName){
+        db.run('INSERT OR IGNORE INTO searchIndex(name, type, path) VALUES ("' + functionName + '", "Function", "R.html#' + functionName + '");');
     });
 });
