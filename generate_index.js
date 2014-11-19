a85056d3b591951fc66e80a63412898bbbd55ec9
Alex Bepple <alex@bepple.de>
2014-11-19 23:07:52 +0100

    pass all paths as parameters

--- generate_index.js
+++ generate_index.js
@@ -4,10 +4,7 @@ var q = require('q');
 var qq = require('./src/q_sugar');
 var sqlite3 = require('sqlite3');
 
-var indexPath = process.argv[2];
-var apiPagePath = process.argv[3];
-
-var createIndex = function () {
+var createIndex = function (indexPath) {
     var db = new sqlite3.Database(indexPath);
     var deferred = q.defer();
     db.serialize(function(){
@@ -30,15 +27,21 @@ var extractFunctionNames = function (apiHtml) {
     return functionNames;
 };
 
-var writeFunctionNamesToIndex = function (db, functionNames) {
+var writeFunctionNamesToIndex = function (functionNames, db) {
     var stmt = db.prepare('INSERT INTO searchIndex(name, type, path) VALUES (?, "Function", ?)');
     functionNames.forEach(function(functionName){
         stmt.run(functionName, 'R.html#'+functionName);
     });
 };
 
-var getFunctionNames = qq.reduce(apiPagePath, readFile, extractFunctionNames);
+var getFunctionNames = function (apiPagePath) {
+    return qq.reduce(apiPagePath, readFile, extractFunctionNames);
+};
 
-qq.all(createIndex, getFunctionNames)
+
+var indexPath = process.argv[2];
+var apiPagePath = process.argv[3];
+
+qq.all(getFunctionNames(apiPagePath), createIndex(indexPath))
 .spread(writeFunctionNamesToIndex);
 
--- src/q_sugar.js
+++ src/q_sugar.js
@@ -5,13 +5,14 @@ var reduce = function (initial) {
 	return r.lPartial(r.reduce, q.when, initial, r.tail(arguments));
 };
 
-var exec = function (fn) {
-	return fn.apply(null);
+var execOrReturn = function (fn) {
+	if (typeof fn === 'function') return fn.apply(null);
+	return fn;
 };
 var all = function () {
-	var fnNames = arguments;
-	var promiseAllResults = r.compose(q.all, r.map(exec));
-	return promiseAllResults(fnNames);
+	var fnNamesAndValues = arguments;
+	var promiseAllResults = r.compose(q.all, r.map(execOrReturn));
+	return promiseAllResults(fnNamesAndValues);
 };
 
 module.exports = {
